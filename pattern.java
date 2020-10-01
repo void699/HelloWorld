8e506e5cd1e6813770893d804835c8e865e49ebb
ayush10000 <72183127+ayush10000@users.noreply.github.com>
2020-10-01 00:28:12 -0700

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -5,7 +5,7 @@ class pattern1
     {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
-        for(int i=1; i=n; i++)
+        for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=n-i; j++)
             {
