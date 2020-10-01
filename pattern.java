e42bab5b2eb21a90d99b040719b4365aa44652ea
void699 <72187200+void699@users.noreply.github.com>
2020-10-01 00:27:10 -0700

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class pattern1
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for(int i=1; i=n; i++)
+        {
+            for(int j=1; j<=n-i; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++)
+            {
+                System.out.println("x  ");
+        
+            }
+            
+        System.out.println();
+    }
+        
+        
+        
+    }
+}
